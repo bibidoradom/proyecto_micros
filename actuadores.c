8ff41244c74d710180e8bf7df603de1148e907c5
Isabel <202301114@alu.comillas.edu.com>
2026-04-15 12:55:57 +0200

    actuadores

--- actuadores.c
+++ actuadores.c
@@ -5,34 +5,36 @@
 void initActuadores(void); //VALORAR SI QUITARLA
 
 void bombaON(void){
-    LATA |= (1 << PIN_RELE_BOMBA); 
-}
-void bombaOFF(void){
     LATA &= ~(1 << PIN_RELE_BOMBA); 
+}
+void bombaOFF(void){ 
+    LATA |= (1 << PIN_RELE_BOMBA);
 }
 
 void valvulaON(void){
-    LATA |= (1 << PIN_RELE_ELECTROVALVULA); 
+    LATC &= ~(1 << PIN_RELE_ELECTROVALVULA);
 }
 
 void valvulaOFF(void){
-    LATA &= ~(1 << PIN_RELE_ELECTROVALVULA);
+    LATC |= (1 << PIN_RELE_ELECTROVALVULA); 
 }
 
 int motorON(void){
-    LATA |= (1 << PIN_RELE_MOTOR);
+    LATCCLR = 1 << PIN_RELE_MOTOR;
+    
 }
 
 int motorOFF(void){
-    LATA &= ~(1 << PIN_RELE_MOTOR);
+    LATCSET = 1 << PIN_RELE_MOTOR;
+    
 }
 
 int calefactorON(void){
-    LATA |= (1 << PIN_RELE_CALEFACTOR); 
+    LATC &= ~(1 << PIN_RELE_CALEFACTOR);
 }
 
 int calefactorOFF(void){
-    LATA &= ~(1 << PIN_RELE_CALEFACTOR);
+   LATC |= (1 << PIN_RELE_CALEFACTOR);
 }
 
 int apagarTodo(void){
